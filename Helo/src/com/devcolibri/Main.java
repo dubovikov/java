766b29bd001b35342925479b4697493296544b22
Dubovikov <gekis2014@gmail.com>
2016-10-17 20:04:52 +0300

    new

---
+++ Helo/src/com/devcolibri/Main.java
@@ -0,0 +1,11 @@
+package com.devcolibri;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+        n();
+    }
+    static void n(){
+        System.out.println("Not work!!!");
+    }
+}
